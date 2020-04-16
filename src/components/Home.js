aa523c7a0b885bbda4cb62972fce35d18fbf3258
asathiya007 <akshay.sathiya@gmail.com>
2020-04-16 19:42:39 -0400

    pushing first iteration of client side app

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                 <Button variant="warning" className="f4 ph5 dib w-30 mv2 mh2" href="/orderhist">Order History</Button>
                 <Button variant="warning" className="f4 ph5 dib w-30 mv2 mh2" href="/currinfo">View Current Info</Button>
                 <Button variant="warning" className="f4 ph5 dib w-30 mv2 mh2" href="/mngft">Manage Food Truck</Button>
-                <Button variant="warning" className="f4 ph5 dib w-30 mv2 mh2" href="/ftsummary">View Food Truck Summary</Button>
+                <Button variant="warning" className="f4 ph5 dib w-30 mv2 mh2" href="/ftsumm">View Food Truck Summary</Button>
                 <Button variant="warning" className="f4 ph5 dib w-30 mv2 mh2" href="/mngbldgstn">Manage Building and Station</Button>
                 <Button variant="warning" className="f4 ph5 dib w-30 mv2 mh2" href="/mngfood">Manage Food</Button>
             </div>
